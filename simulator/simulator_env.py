fixing bugs, now stuck at Broadcasting

simulator/simulator_env.py
@@ -585,7 +585,7 @@ class Simulator:
             weight_array = np.ones(len(sampled_requests))  # rl for matching
             column_name = ['order_id', 'origin_id', 'origin_lat', 'origin_lng', 'dest_id', 'dest_lat', 'dest_lng',
                 'trip_distance', 'start_time', 'origin_grid_id', 'dest_grid_id', 'itinerary_node_list',
-                'itinerary_segment_dis_list', 'trip_time', 'designed_reward', 'cancel_prob', 'accept_premium']
+                'itinerary_segment_dis_list', 'trip_time', 'designed_reward', 'cancel_prob']
             if len(sampled_requests) > 0:
                 itinerary_segment_dis_list = []
                 itinerary_node_list = np.array(sampled_requests)[:, 11]
@@ -610,7 +610,7 @@ class Simulator:
                         print(e)
                         print(itinerary_node)
 
-               
+                # FIXME: Premium: since data it self only has 16 columns, let's remove the last column from column_name first
                 wait_info = pd.DataFrame(sampled_requests, columns=column_name)
                 wait_info['itinerary_node_list'] = itinerary_node_list
                 wait_info['start_time'] = self.time
@@ -1161,7 +1161,7 @@ class Simulator:
 
     #     return self.new_tracks
 
-    def step(self, lr_model, mlp_model, score_agent={}, epsilon=0): # rl for matching
+    def step(self, score_agent={}, epsilon=0): # rl for matching
         """
         This function used to run the simulator step by step
         :return:

simulator/utilities.py
@@ -681,8 +681,8 @@ def order_dispatch_broadcasting(wait_requests, driver_table, maximal_pickup_dist
             premium_driver_loc_array_temp = idle_driver_table.loc[idle_driver_table['premium'] == True, ['lng', 'lat', 'driver_id']]
             num_premium_wait_request = len(premium_request_array_temp)
             num_premium_idle_driver = len(premium_driver_loc_array_temp)
-            premium_request_array = np.repeat(request_array_temp.values, num_premium_idle_driver, axis=0)
-            premium_driver_loc_array = np.tile(driver_loc_array_temp.values, (num_premium_wait_request, 1))
+            premium_request_array = np.repeat(premium_request_array_temp.values, num_premium_idle_driver, axis=0)
+            premium_driver_loc_array = np.tile(premium_driver_loc_array_temp.values, (num_premium_wait_request, 1))
 
             premium_dis_array = distance_array(premium_request_array[:, :2], premium_driver_loc_array[:, :2])
 